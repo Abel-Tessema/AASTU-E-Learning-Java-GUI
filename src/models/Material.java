aa62c79d82c1a4e5478cdb455132c177500e30e7
Bela Jash <abelxotessema@gmail.com>
2024-06-16 21:03:50 +0300

    Add getters and setters for Material.

--- src/models/Material.java
+++ src/models/Material.java
@@ -17,4 +17,40 @@ public class Material {
         this.filePath = filePath;
         this.courseId = courseId;
     }
+
+    // ===== Getters =====
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    // ===== Setters =====
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
 }
